b2f8bcfd79ea7174f1c5be3ec608de9ee1d09ace
faizan136 <faizanmohammadkhadri@gmail.com>
2022-03-06 22:50:48 +0530

    Refactor

--- src/ProductItem.js
+++ src/ProductItem.js
@@ -4,49 +4,71 @@
 //     Add to cart
 //     <i className ='fa fa-shopping-cart'></i>
 //   </button> :null
-import ShouldRender from './utils/ShouldRender'
-const CartButton=({product,onAddClick})=>product.inStock &&
- 
-  <button disabled={!product.inStock} className =" btn btn-danger btn-sm" onClick={onAddClick}>
-    Add to cart
-    <i className ='fa fa-shopping-cart'></i>
-  </button> 
-
+import ShouldRender from "./utils/ShouldRender";
+import IfElse from "./utils/IfElse";
+const CartButton = ({ product, onAddClick }) =>
+  product.inStock && 
+    <button
+      disabled={!product.inStock}
+      className=" btn btn-danger btn-sm"
+      onClick={onAddClick}
+    >
+      Add to cart
+      <i className="fa fa-shopping-cart"></i>
+    </button>
+  ;
 
 //presentation component
 
-const ProductItem =({product,onAdd}) =>{      //destructuring
+const ProductItem = ({ product, onAdd }) => {
+  //destructuring
 
-  const onAddClick =()=>{
-    onAdd(product)
-  }
-    
-return <div class="col-md-3">
-      <div className ='card'>
-      <img className='card-img-top' src={product.img}/>
-      <div className ="card-body">
-        <h4 className ="card-title">{product.brand}{product.model}</h4>
-        <b>${product.price} </b>
-        <h6>
-          <label>In Stock?</label>
-          <input disabled type='checkbox' checked={product.inStock}/></h6>
-        {/* <h6>inS=>tock?:{product.inStock ?"Yes":"No"}</h6> */}
+  const onAddClick = () => {
+    onAdd(product);
+  };
+
+  return (
+    <div class="col-md-3">
+      <div style={{ margin: "20px" }} className="card">
+        <div className="card-header">
+          <span className="card-title">
+            {product.brand}
+            {product.model}
+          </span>
         </div>
+        <img className="card-img-top" src={product.img} />
+        <div className="card-body">
+          <b>${product.price} </b>
+          <h6>
+            <label>In Stock?</label>
+            <input disabled type="checkbox" checked={product.inStock} />
+          </h6>
+          {/* <h6>inS=>tock?:{product.inStock ?"Yes":"No"}</h6> */}
+        </div>
+        {/* <ShouldRender cond={product.inStock}> */}
         <div className="card-footer">
-          <ShouldRender cond={product.inStock}>
-            <button disabled={!product.inStock} className =" btn btn-danger btn-sm" onClick={onAddClick}>
-    Add to cart
-    <i className ='fa fa-shopping-cart'></i>
-  </button> 
-          </ShouldRender>
-         {/* <CartButton product={product} onAddClick={onAddClick}/> */}
+          {/* {product.inStock ? ( */}
+          <IfElse cond={product.instock}>
+            <button className=" btn btn-danger btn-sm" onClick={onAddClick}>
+              Add to cart
+              <i className="fa fa-shopping-cart"></i>
+            </button>
+            
+            <button className=" btn btn-danger btn-sm" onClick={onAddClick}>
+              Notify
+              <i className="fa fa-bell"></i>
+            </button>
+          </IfElse>
+          {/* <CartButton product={product} onAddClick={onAddClick}/> */}
           {/* {product.inStock?
           <button disabled={!product.inStock} className =" btn btn-danger btn-sm" onClick={onAddClick}>
-            Add to cart
-            <i className ='fa fa-shopping-cart'></i>
-          </button> :null} */}
-          </div>
+          Add to cart
+          <i className ='fa fa-shopping-cart'></i>
+        </button> :null} */}
         </div>
-        </div>
-}
+        {/* </ShouldRender> */}
+      </div>
+    </div>
+  );
+};
 export default ProductItem;
